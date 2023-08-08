9fe8fa7c78d337eab60f16566b52e2db6a40e73a
Calum Young <calummyoung@gmail.com>
2023-08-08 23:39:50 +0100

    Split dependancy update

--- scripts/update_dependencies.py
+++ scripts/update_dependencies.py
@@ -10,6 +10,45 @@ from packaging.requirements import Requirement
 from packaging.version import Version
 
 
+def get_new_versions(depencancy: Requirement) -> list[Version] | int:
+    """Get new versions of depencancy from PYPI."""
+    print(f"Checking: {depencancy.name}")
+    specs = list(depencancy.specifier)
+    if len(specs) != 1:
+        print(
+            "Number of specifiers found does not match expected number for"
+            f" {depencancy.name}",
+        )
+        return 1
+
+    spec = specs[0]
+    if spec.operator != "==":
+        print(f"Operator for {depencancy.name} is not ==, please fix and try again")
+        return 1
+
+    current_version = Version(spec.version)
+    print(f"Current version: {current_version}")
+
+    # Get all versions of depencancy from PYPI
+    try:
+        resp = requests.get(
+            f"https://pypi.org/pypi/{depencancy.name}/json",
+            timeout=0.1,
+        )
+    except ConnectionError:
+        print("Unable to connect to PYPI")
+        return 1
+
+    if resp.status_code != 200:
+        raise RuntimeError
+
+    versions = [Version(release) for release in resp.json()["releases"]]
+    versions = [v for v in versions if v > current_version]
+    versions.sort()
+
+    return versions
+
+
 def main() -> int:
     """Update dependencies."""
     pyproject_toml = Path(__file__).parent.parent / "pyproject.toml"
@@ -29,31 +68,9 @@ def main() -> int:
     output_deps = []
     for requirement in deps:
         depencancy = Requirement(requirement)
-        print(f"Checking: {depencancy.name}")
-        specs = list(depencancy.specifier)
-        if len(specs) != 1:
-            print(
-                "Number of specifiers found does not match expected number for"
-                f" {depencancy.name}",
-            )
-            return 1
-
-        spec = specs[0]
-        if spec.operator != "==":
-            print(f"Operator for {depencancy.name} is not ==, please fix and try again")
-            return 1
-
-        current_version = Version(spec.version)
-        print(f"Current version: {current_version}")
-
-        # Get all versions of depencancy from PYPI
-        resp = requests.get(f"https://pypi.org/pypi/{depencancy.name}/json", timeout=5)
-        if resp.status_code != 200:
-            raise RuntimeError
-
-        versions = [Version(release) for release in resp.json()["releases"]]
-        versions = [v for v in versions if v > current_version]
-        versions.sort()
+        versions = get_new_versions(depencancy)
+        if isinstance(versions, int):
+            return versions
 
         # Check if new versions are available
         if len(versions) == 0:
